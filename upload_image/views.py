7b456d9c801e00a44d2ffd1dd556f5d70843b10f
Matheus-Pedro <matheus.caprioli.pedro@gmail.com>
2024-04-20 19:03:32 -0300

    Basic Layout Django

---
+++ upload_image/views.py
@@ -0,0 +1,17 @@
+from django.shortcuts import render
+import os
+from PIL import Image
+from django.conf import settings
+
+# Create your views here.
+def upload_image(request):
+    if request.method == "GET":
+        return render (request, "upload_image.html")
+    elif request.method == "POST":
+        file = request.FILES.get('the_file')
+        img = Image.open(file)
+        path = os.path.join(settings.BASE_DIR, f'media/{file.name}')
+        img = img.save(path)
+
+        print(file)
+        return render(request, "upload_image_concluded.html")
